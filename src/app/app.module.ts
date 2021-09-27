7f4d2e20d07bdcae7440bdfed541dc2bc0c69852
Zuhairah Zainal <19FTT1152@student.pb.edu.bn>
2021-09-27 17:58:29 +0800

    add and import firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+//env
+import { environment } from '../environments/environment';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule,
+          AngularFireModule.initializeApp(environment.firebaseConfig),
+          AngularFireAuthModule,
+          AngularFireDatabaseModule,
+          AngularFireStorageModule,
+          AngularFirestoreModule,
+          IonicModule.forRoot(),
+          AppRoutingModule],
+providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
